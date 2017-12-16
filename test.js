0f5eb553e740f975e19cc65e8bc726d73d5f8a49
James Newton <james.lexogram@gmail.com>
2017-12-16 18:10:25 +0300

    Create test.js

---
+++ test.js
@@ -0,0 +1,29 @@
+;(function test(){
+
+  window.log = function log(message) {   
+    let pre = document.querySelector("pre.scroll")
+
+    pre.innerHTML += message + "\n"
+  }
+
+  let dim = window.dimensions
+
+  dim.addEventListener("resize", resize)
+  dim.addEventListener("keyboardchange", keyboardchange)
+  dim.addEventListener("focuschange", focuschange)
+
+  function resize(dimensions, orientation) {
+    let message =  orientation + " " + dimensions.width + "x" + dimensions.height
+    log (message)
+  }
+
+  function keyboardchange(state, dimensions) {
+    let message =  state + " " + dimensions.width + "x" + dimensions.height
+    log (message)
+  }
+
+  function focuschange(element, elementType) {
+    let message = "focus: " + elementType  
+    log (message)
+  }
+})()
